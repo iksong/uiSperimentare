17cca490f1a69113a5e124220319c138d75b4467
IK <IK@ik-mbp-2.attlocal.net>
2022-02-15 14:56:38 -0600

    where to and not sure views

--- Sperimentare/Shared/ContentView.swift
+++ Sperimentare/Shared/ContentView.swift
@@ -8,15 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var searchText = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        // testing git --- change
+        ScrollView {
+            VStack {
+                WhereToSearchBar(searchText: $searchText)
+                NotSureView()
+            }
+        }
+        .background(Color.yellow)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct PlaceHolder<T: View>: ViewModifier {
+    var placeHolder: T
+    var show: Bool
+    func body(content: Content) -> some View {
+        ZStack(alignment: .leading) {
+            if show { placeHolder }
+            content
+        }
+    }
+}
+
+extension View {
+    func placeHolder<T: View>(_ holder: T, show: Bool) -> some View {
+        self.modifier(PlaceHolder(placeHolder: holder, show: show))
+    }
+}
+
+struct WhereToSearchBar: View {
+    @Binding var searchText: String
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+            HStack(alignment: .center) {
+                Spacer(minLength: 30)
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(Color.pink)
+                TextField("", text: $searchText) { _ in
+                    print("Search - editing")
+                } onCommit: {
+                    print("Search - done")
+                }
+                .font(.headline)
+                .foregroundColor(.gray)
+                .placeHolder(placeHolderText, show: searchText.isEmpty)
+            }
+        }
+        .frame(height: 60)
+        .cornerRadius(25)
+        .padding()
+    }
+    
+    var placeHolderText: some View {
+        Text("Where are you going?")
+            .font(.headline)
+            .foregroundColor(.black)
+    }
+}
+
+struct NotSureView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.yellow, .teal], startPoint: .top, endPoint: .bottom)
+
+            VStack(spacing: 40) {
+                Text("Not sure where to go? \nPerfect.")
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    .font(.system(size: 30, weight: .bold))
+                
+                Button {
+                    print("flexible clicked")
+                } label: {
+                    Text("I'm flexible")
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.teal)
+                        .frame(width: 150, height: 80, alignment: .center)
+                        .background(.white)
+                        .cornerRadius(60)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, idealHeight: 400, maxHeight: 400, alignment: .center)
     }
 }
